a15c7a185cabdb4711c38027e8f4651c16e20f97
HuynhTienThan <huynhtienthan3@gmail.com>
2022-10-18 23:30:21 +0700

    hoanthanh

--- js/main.js
+++ js/main.js
@@ -28,31 +28,6 @@ setInterval (chuyenDoi,3000)
 //Tìm Kiếm Sản Phẩm
 var listClassPhone=[".list-phone .phone1",".list-phone .phone2",".list-phone .phone3",".list-phone .phone4",".list-phone .phone5",".list-phone .phone6",".list-phone .phone7",".list-phone .phone8",]
 
-// document.querySelector('.search-box').onclick = function(){
-//     var value=document.querySelector('#txtSearch').value
-//     console.log(value)
-//     document.querySelector('#txtSearch').value=""
-//     var timThay=false
-//     var listPhone=["Xiaomi Redmi Note 7 (64GB)","Samsung Galaxy A50 (64GB)","iPhone 6s Plus (32GB)","Samsung Galaxy A10","OPPO A5s","Vivo Y15","iPhone Xs Max (64GB)","iPhone X (256GB)"]
-//     var listClassPhone=[".list-phone .phone1",".list-phone .phone2",".list-phone .phone3",".list-phone .phone4",".list-phone .phone5",".list-phone .phone6",".list-phone .phone7",".list-phone .phone8",]
-//     for (var i = 0;i<listPhone.length;i++){
-//         if(listPhone[i].includes(value)){
-//             document.querySelector(listClassPhone[i]).setAttribute("style","display:block;")
-//             timThay=true
-//         }else{
-//             document.querySelector(listClassPhone[i]).setAttribute("style","display:none;")
-            
-//         }
-//     }
-//     if(!timThay){
-//         document.querySelector('.report_text').setAttribute("style","display:block;")
-//         document.querySelector('.report_text').innerHTML='Rất tiếc, chúng tôi không tìm thấy kết quả nào phù hợp với từ khóa "'+"<strong>"+value+'"</strong>'
-//     }else{
-//         document.querySelector('.report_text').setAttribute("style","display:none;")
-
-//     }
-// }
-
 function TimKiemSanPham(){
     var value=document.querySelector('#txtSearch').value
     document.querySelector('#txtSearch').value=""
@@ -199,6 +174,43 @@ oppo.onclick = function(){
     }
 }
 
+//đếm số lượng hàng hóa
+// document.querySelector('.list-phone .information .buy1').onclick=function(){
+//     document.querySelector('.gio-hang').innerHTML=document.querySelector('.gio-hang').innerHTML+'<div class="row"><div class="col san-pham">Xiaomi Redmi Note 7 (64GB)</div><div class="col gia">4.990.000₫</div><div class="col so-luong">1</div><div class="col chon">Xóa</div></div>'
+// }
+// document.querySelector('.list-phone .information .buy2').onclick=function(){
+//     document.querySelector('.gio-hang').innerHTML=document.querySelector('.gio-hang').innerHTML+'<div class="row"><div class="col san-pham">Samsung Galaxy A50 (64GB)</div><div class="col gia">6.990.000₫</div><div class="col so-luong">1</div><div class="col chon">Xóa</div></div>'
+// }
+// document.querySelector('.list-phone .information .buy3').onclick=function(){
+//     document.querySelector('.gio-hang').innerHTML=document.querySelector('.gio-hang').innerHTML+'<div class="row"><div class="col san-pham">iPhone 6s Plus (32GB)</div><div class="col gia">9.990.000₫</div><div class="col so-luong">1</div><div class="col chon">Xóa</div></div>'
+// }
+// document.querySelector('.list-phone .information .buy4').onclick=function(){
+//     document.querySelector('.gio-hang').innerHTML=document.querySelector('.gio-hang').innerHTML+'<div class="row"><div class="col san-pham">Samsung Galaxy A10</div><div class="col gia">3.090.000₫</div><div class="col so-luong">1</div><div class="col chon">Xóa</div></div>'
+// }
+// document.querySelector('.list-phone .information .buy5').onclick=function(){
+//     document.querySelector('.gio-hang').innerHTML=document.querySelector('.gio-hang').innerHTML+'<div class="row"><div class="col san-pham">OPPO A5s</div><div class="col gia">3.990.000₫</div><div class="col so-luong">1</div><div class="col chon">Xóa</div></div>'
+// }
+// document.querySelector('.list-phone .information .buy6').onclick=function(){
+//     document.querySelector('.gio-hang').innerHTML=document.querySelector('.gio-hang').innerHTML+'<div class="row"><div class="col san-pham">Vivo Y15</div><div class="col gia">4.990.000₫</div><div class="col so-luong">1</div><div class="col chon">Xóa</div></div>'
+// }
+// document.querySelector('.list-phone .information .buy7').onclick=function(){
+//     document.querySelector('.gio-hang').innerHTML=document.querySelector('.gio-hang').innerHTML+'<div class="row"><div class="col san-pham">iPhone Xs Max (64GB)</div><div class="col gia">29.990.000₫</div><div class="col so-luong">1</div><div class="col chon">Xóa</div></div>'
+// }
+// document.querySelector('.list-phone .information .buy8').onclick=function(){
+//     document.querySelector('.gio-hang').innerHTML=document.querySelector('.gio-hang').innerHTML+'<div class="row"><div class="col san-pham">iPhone X (256GB)</div><div class="col gia">29.990.000₫</div><div class="col so-luong">1</div><div class="col chon">Xóa</div></div>'
+// }
+// document.querySelector('.head-search .shop').onclick=function(){
+//     gioHang=document.querySelector('.gio-hang')
+//     console.log(gioHang.style.display)
+//     if(gioHang.style.display=="none"){
+//         gioHang.setAttribute("style","display:block;")
+//     }
+//     else if(gioHang.style.display=="block"){
+//         gioHang.setAttribute("style","display:none;")
+//     }
+    
+// }
+
 //mobile
 function TimKiemSanPhamMobile(){
     var value_mobile=document.querySelector('#txtSearch-mobile').value
@@ -227,15 +239,3 @@ function TimKiemSanPhamMobile(){
     }
  
 }
-// document.querySelector('.search-icon-mobile').onlick=TimKiemSanPhamMobile()
-// $('.search-text-mobile').focus(function(){
-//     $(document).keypress(function(event){
-//             var keycode = (event.keyCode ? event.keyCode : event.which);
-//             if (keycode == '13') {
-//                 document.querySelector('.search-text-mobile').style.display='none'
-//                 document.querySelector('.search-icon-mobile').style.display='block'
-//                 TimKiemSanPhamMobile()
-//                 return
-//             }
-//         });
-// })
